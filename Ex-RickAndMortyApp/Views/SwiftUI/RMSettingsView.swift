58ecda3fa1f05f494139059236f556f29bedf568
HALUK BAYRAKCI <halukbayrakci32@gmail.com>
2023-09-14 18:57:17 +0300

    Update Settings Detail and Added SwiftUI View

---
+++ Ex-RickAndMortyApp/Views/SwiftUI/RMSettingsView.swift
@@ -0,0 +1,47 @@
+//
+//  RMSettingsView.swift
+//  Ex-RickAndMortyApp
+//
+//  Created by HALUK BAYRAKCI on 14.09.2023.
+//
+
+import SwiftUI
+
+struct RMSettingsView: View {
+    
+    let viewModel: RMSettingsViewViewModel
+    
+    init(viewModel: RMSettingsViewViewModel) {
+        self.viewModel = viewModel
+    }
+    
+    var body: some View {
+        List(viewModel.cellViewModel) { viewModel in
+            HStack {
+                if let image = viewModel.image {
+                    Image(uiImage: image)
+                        .resizable()
+                        .renderingMode(.template)
+                        .foregroundColor(Color.white)
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 20, height: 20)
+                        .foregroundColor(Color.red)
+                        .padding(8)
+                        .background(Color(viewModel.iconContainerColo))
+                        .cornerRadius(6)
+                }
+                Text(viewModel.title)
+                    .padding(.leading, 10)
+            }
+            .padding(.bottom, 3)
+        }
+    }
+}
+
+struct RMSettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        RMSettingsView(viewModel: .init(cellViewModel: RMSettingsOption.allCases.compactMap({
+            return RMSettingsCellViewModel(type: $0)
+        })) )
+    }
+}
