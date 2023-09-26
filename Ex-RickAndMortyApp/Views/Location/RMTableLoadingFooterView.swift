7c17a915998644b1ca190d8c324fd21e9eee4c74
HALUK BAYRAKCI <halukbayrakci32@gmail.com>
2023-09-26 18:15:15 +0300

    Wrap up location pagination + device families

---
+++ Ex-RickAndMortyApp/Views/Location/RMTableLoadingFooterView.swift
@@ -0,0 +1,20 @@
+//
+//  RMTableLoadingFooterView.swift
+//  Ex-RickAndMortyApp
+//
+//  Created by HALUK BAYRAKCI on 26.09.2023.
+//
+
+import UIKit
+
+class RMTableLoadingFooterView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
